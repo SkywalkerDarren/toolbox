8a769a193e02e72d8546a3340dbe28bfff0ce31e
Darren <yh322yh@163.com>
2017-11-11 15:29:03 +0800

    重构代码

--- src/com/ToolBox/UI/LiteralCodeUI.java
+++ src/com/ToolBox/UI/LiteralCodeUI.java
@@ -18,37 +18,124 @@ class LiteralCodeUI extends JPanel {
     private static TextBox leftTextArea;
     private static TextBox rightTextArea;
     private JTextField textFieldRoute;
+    private JComboBox<String> fromComboBox;
+    private JComboBox<String> toComboBox;
+    private JButton btnExchange;
+    private JButton btnChooseFile;
+    private JButton btnExchangeFile;
+    private JLabel lblFrom;
+    private JLabel lblTo;
+    private JLabel lblLeftCode;
+    private JLabel lblRightCode;
 
+    /**
+     * 初始化组件
+     */
+    private void initCompoment() {
+        btnChooseFile = new JButton();
+        btnExchangeFile = new JButton();
+        textFieldRoute = new JTextField();
+        fromComboBox = new JComboBox<>();
+        toComboBox = new JComboBox<>();
+        leftTextArea = new TextBox(input);
+        rightTextArea = new TextBox(output);
+        btnExchange = new JButton();
+        lblTo = new JLabel();
+        lblFrom = new JLabel();
+        lblLeftCode = new JLabel();
+        lblRightCode = new JLabel();
+    }
 
-    LiteralCodeUI() {
-        setOpaque(false);
-
-        JComboBox<String> fromComboBox;
-        JComboBox<String> toComboBox;
-        JButton btnExchange;
-        JButton btnChooseFile;
-        JButton btnExchangeFile;
-        JLabel lblFrom;
-        JLabel lblTo;
-        JLabel lblLeftCode;
-        JLabel lblRightCode;
-
-        /***
-         * 构建文字编码转换框架
-         */
-
+    /**
+     * 初始化布局
+     */
+    private void initUI() {
         final Color normal = new Color(245, 255, 255);
         final Color button = new Color(240, 255, 250);
         final Font fontPlain = new Font("微软雅黑", Font.PLAIN, 14);
 
         //选择文件按钮
-        btnChooseFile = new JButton();
         btnChooseFile.setOpaque(false);
         btnChooseFile.setText(chooseFile);
         btnChooseFile.setFont(fontPlain);
         btnChooseFile.setBounds(84, 50, 105, 35);
         btnChooseFile.setBackground(button);
-        super.add(btnChooseFile);
+        add(btnChooseFile);
+
+        //最上方转换按钮
+        btnExchangeFile.setOpaque(false);
+        btnExchangeFile.setText(exchangeTip);
+        btnExchangeFile.setFont(fontPlain);
+        btnExchangeFile.setBounds(596, 50, 105, 35);
+        btnExchangeFile.setBackground(button);
+        add(btnExchangeFile);
+
+        //文件路径文本框
+        textFieldRoute.setBackground(normal);
+        textFieldRoute.setEditable(false);
+        textFieldRoute.setText(route);
+        textFieldRoute.setBounds(213, 50, 375, 35);
+        textFieldRoute.setFont(fontPlain);
+        textFieldRoute.setForeground(Color.GRAY);
+        add(textFieldRoute);
+
+        //左
+        fromComboBox.setOpaque(false);
+        fromComboBox.setFont(fontPlain);
+        fromComboBox.setBounds(92, 108, 205, 35);
+        fromComboBox.setBackground(normal);
+        add(fromComboBox);
+
+        //右
+        toComboBox.setOpaque(false);
+        toComboBox.setFont(fontPlain);
+        toComboBox.setBounds(490, 108, 205, 35);
+        toComboBox.setBackground(normal);
+        add(toComboBox);
+
+        leftTextArea.setBounds(60, 170, 275, 315);
+        add(leftTextArea);
+
+        rightTextArea.setBounds(455, 170, 275, 315);
+        add(rightTextArea);
+
+        //转换按钮
+        btnExchange.setOpaque(false);
+        btnExchange.setText(exchange);
+        btnExchange.setFont(fontPlain);
+        btnExchange.setBounds(342, 305, 105, 35);
+        btnExchange.setBackground(button);
+        add(btnExchange);
+
+        //标签从
+        lblFrom.setText(from);
+        lblFrom.setFont(fontPlain);
+        lblFrom.setBounds(70, 116, 20, 20);
+        add(lblFrom);
+
+        //标签转换到
+        lblTo.setText(to);
+        lblTo.setFont(fontPlain);
+        lblTo.setBounds(440, 116, 60, 20);
+        add(lblTo);
+
+        //编码标签
+        lblLeftCode.setText(code);
+        lblLeftCode.setFont(fontPlain);
+        lblLeftCode.setBounds(302, 114, 60, 20);
+        add(lblLeftCode);
+
+        //编码标签
+        lblRightCode.setText(code);
+        lblRightCode.setFont(fontPlain);
+        lblRightCode.setBounds(702, 114, 60, 20);
+        add(lblRightCode);
+    }
+
+    /**
+     * 建立监听事件
+     */
+    private void createAction() {
         btnChooseFile.addActionListener(e -> {
             // TODO Auto-generated method stub
             if (!(textFieldRoute.getText().equals(route))) {
@@ -56,98 +143,32 @@ class LiteralCodeUI extends JPanel {
             }
         });
 
-        //最上方转换按钮
-        btnExchangeFile = new JButton();
-        btnExchangeFile.setOpaque(false);
-        btnExchangeFile.setText(exchangeTip);
-        btnExchangeFile.setFont(fontPlain);
-        btnExchangeFile.setBounds(596, 50, 105, 35);
-        btnExchangeFile.setBackground(button);
-        super.add(btnExchangeFile);
-
-        //文件路径文本框
-        textFieldRoute = new JTextField();
-        textFieldRoute.setBackground(normal);
-        textFieldRoute.setEditable(false);
-        textFieldRoute.setText(route);
-        textFieldRoute.setBounds(213, 50, 375, 35);
-        textFieldRoute.setFont(fontPlain);
-        textFieldRoute.setForeground(Color.GRAY);
-        super.add(textFieldRoute);
-
-        //左
-        fromComboBox = new JComboBox<>();
-        fromComboBox.setOpaque(false);
-        fromComboBox.setFont(fontPlain);
-        fromComboBox.setBounds(92, 108, 205, 35);
-        fromComboBox.setBackground(normal);
         fromComboBox.addActionListener(e -> {
             // TODO Auto-generated method stub
 
         });
-        super.add(fromComboBox);
 
-        //右
-        toComboBox = new JComboBox<>();
-        toComboBox.setOpaque(false);
-        toComboBox.setFont(fontPlain);
-        toComboBox.setBounds(490, 108, 205, 35);
-        toComboBox.setBackground(normal);
         toComboBox.addActionListener(e -> {
             // TODO Auto-generated method stub
 
         });
-        super.add(toComboBox);
 
-        leftTextArea = new TextBox(input);
-        leftTextArea.setBounds(60, 170, 275, 315);
-        super.add(leftTextArea);
 
-        rightTextArea = new TextBox(output);
-        rightTextArea.setBounds(455, 170, 275, 315);
-        super.add(rightTextArea);
-
-        //转换按钮
-        btnExchange = new JButton();
-        btnExchange.setOpaque(false);
-        btnExchange.setText(exchange);
-        btnExchange.setFont(fontPlain);
-        btnExchange.setBounds(342, 305, 105, 35);
-        btnExchange.setBackground(button);
-        super.add(btnExchange);
         btnExchange.addActionListener(e -> {//单击转换按钮实现编码转换，字体设置为黑色
             // TODO Auto-generated method stub
             rightTextArea.setForeground(Color.BLACK);
         });
+    }
 
-        //标签从
-        lblFrom = new JLabel();
-        lblFrom.setText(from);
-        lblFrom.setFont(fontPlain);
-        lblFrom.setBounds(70, 116, 20, 20);
-        super.add(lblFrom);
+    /**
+     * 构建文字编码转换框架
+     */
+    LiteralCodeUI() {
+        initCompoment();
+        initUI();
+        createAction();
 
-        //标签转换到
-        lblTo = new JLabel();
-        lblTo.setText(to);
-        lblTo.setFont(fontPlain);
-        lblTo.setBounds(440, 116, 60, 20);
-        super.add(lblTo);
-
-        //编码标签
-        lblLeftCode = new JLabel();
-        lblLeftCode.setText(code);
-        lblLeftCode.setFont(fontPlain);
-        lblLeftCode.setBounds(302, 114, 60, 20);
-        super.add(lblLeftCode);
-
-        //编码标签
-        lblRightCode = new JLabel();
-        lblRightCode.setText(code);
-        lblRightCode.setFont(fontPlain);
-        lblRightCode.setBounds(702, 114, 60, 20);
-        super.add(lblRightCode);
-
+        setOpaque(false);
     }
 
 }
