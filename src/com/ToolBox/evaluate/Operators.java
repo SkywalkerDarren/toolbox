a751693f99d97601b48fa66ed19e55cb9be590cf
Darren <yh322yh@163.com>
2017-11-23 14:09:30 +0800

    增加注释

--- src/com/ToolBox/evaluate/Operators.java
+++ src/com/ToolBox/evaluate/Operators.java
@@ -5,12 +5,22 @@ import ch.obermuhlner.math.big.BigDecimalMath;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.util.EmptyStackException;
 import java.util.HashMap;
 
+/**
+ * 运算符类
+ * 设定各种运算符，大数或长整数的运算
+ *
+ * @author 杨弘
+ */
 class Operators {
     private final static MathContext mc = new MathContext(64, RoundingMode.HALF_UP);
     static HashMap<String, Operator> operatorHashMap = new HashMap<>(32);
+
+    // 运算符初始化
     static {
+        // 中止符
         operatorHashMap.put("@", new Operator() {
             @Override
             public int getAry() {
@@ -28,12 +38,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -55,13 +65,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return a.add(b);
+            public BigDecimal calc(BigDecimal... args) {
+                return args[0].add(args[1]);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a + b;
+            public Long calc(Long... args) {
+                return args[0] + args[1];
             }
         });
 
@@ -82,13 +92,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return a.subtract(b);
+            public BigDecimal calc(BigDecimal... args) {
+                return args[0].subtract(args[1]);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a - b;
+            public Long calc(Long... args) {
+                return args[0] - args[1];
             }
         });
 
@@ -109,13 +119,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return a.multiply(b);
+            public BigDecimal calc(BigDecimal... args) {
+                return args[0].multiply(args[1]);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a * b;
+            public Long calc(Long... args) {
+                return args[0] * args[1];
             }
         });
 
@@ -136,12 +146,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return a.divide(b, 32, BigDecimal.ROUND_HALF_UP);
+            public BigDecimal calc(BigDecimal... args) {
+                return args[0].divide(args[1], 32, BigDecimal.ROUND_HALF_UP);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -163,13 +173,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a * b;
+            public Long calc(Long... args) {
+                return args[0] * args[1];
             }
         });
 
@@ -190,13 +200,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a / b;
+            public Long calc(Long... args) {
+                return args[0] / args[1];
             }
         });
 
@@ -217,13 +227,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a | b;
+            public Long calc(Long... args) {
+                return args[0] | args[1];
             }
         });
 
@@ -244,13 +254,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a & b;
+            public Long calc(Long... args) {
+                return args[0] & args[1];
             }
         });
 
@@ -271,13 +281,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return ~a;
+            public Long calc(Long... args) {
+                return ~args[0];
             }
         });
 
@@ -298,13 +308,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a ^ b;
+            public Long calc(Long... args) {
+                return args[0] ^ args[1];
             }
         });
 
@@ -325,13 +335,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a % b;
+            public Long calc(Long... args) {
+                return args[0] % args[1];
             }
         });
 
@@ -352,13 +362,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a << b;
+            public Long calc(Long... args) {
+                return args[0] << args[1];
             }
         });
 
@@ -379,13 +389,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return a >> b;
+            public Long calc(Long... args) {
+                return args[0] >> args[1];
             }
         });
 
@@ -406,13 +416,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return (a >>> b) | (a << 64 - b);
+            public Long calc(Long... args) {
+                return (args[0] >>> args[1]) | (args[0] << 64 - args[1]);
             }
         });
 
@@ -433,13 +443,13 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
-                return (a << b) | (a >>> 64 - b);
+            public Long calc(Long... args) {
+                return (args[0] << args[1]) | (args[0] >>> 64 - args[1]);
             }
         });
 
@@ -460,12 +470,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return BigDecimalMath.sin(a, mc);
+            public BigDecimal calc(BigDecimal... args) {
+                return BigDecimalMath.sin(args[0], mc);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -487,12 +497,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return BigDecimalMath.cos(a, mc);
+            public BigDecimal calc(BigDecimal... args) {
+                return BigDecimalMath.cos(args[0], mc);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -514,12 +524,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return BigDecimalMath.tan(a, mc);
+            public BigDecimal calc(BigDecimal... args) {
+                return BigDecimalMath.tan(args[0], mc);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -541,16 +551,16 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 try {
-                    return BigDecimalMath.factorial(a.intValueExact());
+                    return BigDecimalMath.factorial(args[0].intValueExact());
                 } catch (Exception e) {
                     throw new IllegalArgumentException("阶乘应为整数");
                 }
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -572,12 +582,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return BigDecimalMath.pow(a, b, mc);
+            public BigDecimal calc(BigDecimal... args) {
+                return BigDecimalMath.pow(args[0], args[1], mc);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -599,12 +609,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return BigDecimalMath.root(b, a, mc);
+            public BigDecimal calc(BigDecimal... args) {
+                return BigDecimalMath.root(args[1], args[0], mc);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -626,12 +636,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return BigDecimalMath.log10(a, mc);
+            public BigDecimal calc(BigDecimal... args) {
+                return BigDecimalMath.log10(args[0], mc);
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -653,12 +663,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return a.divideAndRemainder(b)[1];
+            public BigDecimal calc(BigDecimal... args) {
+                return args[0].divideAndRemainder(args[1])[1];
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -680,12 +690,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
-                return a.abs();
+            public BigDecimal calc(BigDecimal... args) {
+                return args[0].abs();
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -707,12 +717,12 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
@@ -734,18 +744,24 @@ class Operators {
             }
 
             @Override
-            public BigDecimal calc(BigDecimal a, BigDecimal b) {
+            public BigDecimal calc(BigDecimal... args) {
                 return null;
             }
 
             @Override
-            public Long calc(Long a, Long b) {
+            public Long calc(Long... args) {
                 return null;
             }
         });
     }
 }
 
+/**
+ * 运算符抽象类
+ * 定义运算符目数，优先级，名称，大数运算方法，整数运算方法
+ *
+ * @author 杨弘
+ */
 abstract class Operator {
     public abstract int getAry();
 
@@ -753,7 +769,7 @@ abstract class Operator {
 
     public abstract String getName();
 
-    public abstract BigDecimal calc(BigDecimal a, BigDecimal b);
+    public abstract BigDecimal calc(BigDecimal... args) throws EmptyStackException;
 
-    public abstract Long calc(Long a, Long b);
+    public abstract Long calc(Long... args) throws EmptyStackException;
 }
